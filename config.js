16bf72f5401e189aae8ba7d9b05ff9d0df9a885e
finestfaisal <finestfaisal@gmail.com>
2024-11-05 01:42:25 +0300

    Update config.js

--- config.js
+++ config.js
@@ -55,17 +55,17 @@ global.rpg = {
 }
 
 //auto functioner
-global.autoTyping = false;                //make true to enable auto typing
+global.autoTyping = true;                //make true to enable auto typing
 global.autoRecord = false;                //make true to enable auto recording
 global.autoViewStatus = true;       //make true to view statuses
-global.unavailable = true;     
+global.unavailable = false;     
 //to show your real presence  
-global.available = false;   
+global.available = true;   
 // to show always online  
 global.autoreadmessages = false;   
 ///To always read messages
 global.chatbot = false;
-global.autoreact = false;
+global.autoreact = true;
 
 global.prefix = '.';
 
